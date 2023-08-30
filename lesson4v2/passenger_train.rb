3f45a190ee4fc95dc22c466442d221f072e5527c
xaltyr <133250463+xaltyr@users.noreply.github.com>
2023-08-30 22:51:17 +0500

    Delete lesson4v2 directory

--- lesson4v2/passenger_train.rb
+++
@@ -1,9 +0,0 @@
-class PassengerTrain < Train
-  attr_reader :type, :wagons
-
-  def initialize(number)
-    @wagons = []
-    @number = number
-    @type = :passenger
-  end
-end
--- lesson4v2/passenger_wagon.rb
+++
@@ -1,7 +0,0 @@
-class PassengerWagon
-  attr_reader :wagon_type
-
-  def initialize
-    @wagon_type = :passenger
-  end
-end
